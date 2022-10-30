02371d6dd88c13c22bf915a0eec14a79fe7a954d
Smeet711 <76865313+Smeet711@users.noreply.github.com>
2022-10-30 08:37:33 +0530

    Added solution - LeetHub

---
+++ Chicks in a Zoo - GFG/chicks-in-a-zoo.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int N = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            long ans = obj.NoOfChicks(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long NoOfChicks(int N)
+    {
+        // Code here
+        
+        long[] dp = new long[N+1];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        int prev = 0;
+        long sum = 1;
+        
+        for(int i=2;i<=N;i++){
+            if(i >= 6 ){
+                sum -= dp[prev++];
+            }
+            
+            dp[i] = sum * 2;
+            sum += dp[i];
+            
+        }
+        return sum;
+        
+    }
+}
